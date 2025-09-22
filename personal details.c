30562fc4189d7cc42ca1d1f9174a54b2483b61f2
rodgerskilongosi <muasarodgers@gmail.com>
2025-09-22 18:48:02 +0700

    Add files via upload

---
+++ personal details.c
@@ -0,0 +1,35 @@
+//simple c program
+/*
+  Name :Muasa Rodgers Kilongosi
+  Reg  :CT100/G/26245/25
+  Description :personal details
+ */
+#include<stdio.h>
+
+//main function
+int main(){
+
+int Height;                           
+printf("Enter Your Height");
+    scanf("%d",&Height);
+    
+printf("Your Height is %d\n",Height);
+
+ 
+ int Weight ;
+ printf(" Enter Your Weight");
+    
+    scanf("%d",&Weight);
+    
+printf("Your Weight is %d\n",Weight );
+    
+    
+     
+ int contact ;
+ printf(" Enter Your Contact");
+    
+    scanf("%d",&contact);
+    
+printf("Your contact is %d\n",contact);
+return 0;
+}
